a9479a2918a895e66084dffb4972039bedd29a7f
M Rahul Vyas <113542667+rahulvyasm@users.noreply.github.com>
2024-03-14 22:05:01 +0530

    Update and rename us_medical_insurance_cost_prediction.py to medical_insurance_cost_prediction.py

--- medical_insurance_cost_prediction.py
+++ medical_insurance_cost_prediction.py
@@ -1,97 +1,87 @@
-#!/usr/bin/env python
-# coding: utf-8
+# In[1]:
 
-# In[10]:
-
-
-import numpy as np 
+    
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
+import pickle
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.linear_model import LinearRegression, Ridge
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import cross_val_score, train_test_split
 
+# In[2]:
+
+    
+df=pd.read_csv('medical_insurance.csv', header=0)
+print(f"{df.heaf()}")
+
+# In[3]:
+
+    
+print(f"\n{df.info()}")
+
+# In[4]:
+
+    
+print(f"\n{df.describe(include='all')}")
+
+# In[5]:
+
+    
+print(f"\n{df.isna().value_counts()}")
+
+# In[6]:
+
+    
+dummy_vars=pd.get_dummies(df[['sex','smoker','region']])
+print(f"\n{dummy_vars}")
+
+# In[7]:
+
+    
+#renameing the columns
+df.drop('smoker',axis=1,inplace=True)
+
+# In[9]:
+
+    
+dummy_vars=dummy_vars.rename(columns={'sex_female':'female','sex_male':'male','smoker_yes':'smoker','region_northeast':'northeast','region_northwest':'northwest','region_southeast':'southeast','region_southwest':'southwest'})
+dummy_vars.drop('smoker_no', axis=1,inplace=True)
+print(f"\n{dummy_vars}")
+
+# In[10]:
+
+    
+df=pd.concat([df,dummy_vars],axis=1)
+df.drop(['sex','region'],axis=1,inplace=True)
+print(f"\n{df.head()}")
 
 # In[11]:
 
-
-df=pd.read_csv('medical_insurance.csv', header=0)
-df.head()
-
+    
+#using a Regression Plot
+print(f"\n{sns.regplot(x='bmi',y='charges',data=df)}")
 
 # In[12]:
 
-
-df.info()
+    
+#using a Box Plot
+print(f"{sns.boxplot(data=df,x='smoker',y='charges')}")
 
 
 # In[13]:
 
 
-df.describe(include='all')
+df.corr()
 
 
 # In[14]:
 
 
-df.isna().value_counts()
+lm= LinearRegression()
+print(f"{lm}")
 
 
 # In[15]:
-
-
-dummy_vars=pd.get_dummies(df[['sex','smoker','region']])
-dummy_vars
-
-
-# In[16]:
-
-
-#rename the columns
-df.drop('smoker',axis=1,inplace=True)
-
-dummy_vars=dummy_vars.rename(columns={'sex_female':'female','sex_male':'male','smoker_yes':'smoker','region_northeast':'northeast','region_northwest':'northwest','region_southeast':'southeast','region_southwest':'southwest'})
-dummy_vars.drop('smoker_no', axis=1,inplace=True)
-dummy_vars
-
-
-# In[17]:
-
-
-df=pd.concat([df,dummy_vars],axis=1)
-df.drop(['sex','region'],axis=1,inplace=True)
-df.head()
-
-
-# In[18]:
-
-
-sns.regplot(x='bmi',y='charges',data=df)
-
-
-# In[19]:
-
-
-sns.boxplot(data=df,x='smoker',y='charges')
-
-
-# In[20]:
-
-
-df.corr()
-
-
-# In[21]:
-
-
-lm= LinearRegression()
-lm
-
-
-# In[22]:
 
 
 x_data=df[['smoker']]
@@ -101,7 +91,7 @@ lm.fit(x_data,y_data)
 print(f'The R^2 score of the given model is {lm.score(x_data,y_data)}.')
 
 
-# In[23]:
+# In[16]:
 
 
 x_all=df.drop(['charges'],axis=1)
@@ -110,7 +100,7 @@ r2_score1=lm.score(x_all,y_data)
 print(f'The R^2 score of the given model, with all attributes except "smoker" is {r2_score}.')
 
 
-# In[24]:
+# In[17]:
 
 
 Input=[('scale',StandardScaler()),('polynomial',PolynomialFeatures(include_bias=False)),('model',LinearRegression())]
@@ -122,19 +112,19 @@ r2_score2=r2_score(y_data,yhat)
 print(f'The R^2 score of the model after the training pipeline is {r2_score2}.')
 
 
-# In[25]:
+# In[18]:
 
 
 print(f'Here the model performance improved by {r2_score2-r2_score1} after training pipeline.')
 
 
-# In[26]:
+# In[19]:
 
 
 x_train,x_test,y_train,y_test=train_test_split(x_all,y_data,test_size=.2,random_state=1)
 
 
-# In[27]:
+# In[20]:
 
 
 RidgeModel=Ridge(alpha=0.1)
@@ -144,13 +134,13 @@ r2_score3=r2_score(y_test,yhat)
 print(f'The R^2 score of the model after Ridge Regression is {r2_score3}.')
 
 
-# In[28]:
+# In[21]:
 
 
 print(f'Here the model performance degraded by {r2_score3-r2_score2} after training Ridge Regression at alpha=0.1.')
 
 
-# In[29]:
+# In[22]:
 
 
 pr=PolynomialFeatures(degree=2)
@@ -162,24 +152,21 @@ r2_score4=r2_score(y_test,y_hat)
 print(f'The R^2 score of the model after polynomial transformation on Ridge Regression is {r2_score4}.')
 
 
-# In[30]:
+# In[23]:
 
 
 print(f'Here the model performance improved by {r2_score4-r2_score3} after training Ridge Regression at alpha=0.1.')
 
 
-# In[32]:
+# In[24]:
 
 
-import pickle
-
-# Assuming you have already trained your model (RidgeModel in this case)
 # Save the model to disk
 filename = 'insurance_model.pkl'
 pickle.dump(RidgeModel, open(filename, 'wb'))
 
 
-# In[33]:
+# In[25]:
 
 
 pipeline_filename = 'insurance_pipeline.pkl'
